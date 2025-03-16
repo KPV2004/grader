b11567c53a2d7c3777328a28f7a63c5884059c0b
KPV2004 <pannetvanicharkorl@gmail.com>
2025-03-16 13:26:39 +0700

    feat(regex):validation source by regex and find header file c, c++ and return array of string

---
+++ source_validation.go
@@ -0,0 +1,51 @@
+package grader
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func (g *Grader) GetHeaderC_Cpp(fileName string, fileType string) ([]string, error) {
+	if fileType != "c" && fileType != "cpp" {
+		return nil, fmt.Errorf("❌ Error file type %s: %v", fileType)
+	}
+	code, err := os.ReadFile(filepath.Join(g.pathOfSource, fileName+"."+fileType))
+	if err != nil {
+		return nil, fmt.Errorf("❌ Error reading source code %s: %v", fileName, err)
+	}
+	includeRegex := regexp.MustCompile(`#include\s*[<"]([^>"]+)[>"]`)
+
+	matches := includeRegex.FindAllStringSubmatch(string(code), -1)
+
+	// แสดงรายการ Header ที่พบ
+	// fmt.Println("🔍 Header C, C++ in " + fileName + " Files :")
+	// fmt.Println(len(matches))
+	var headerList []string
+	for _, match := range matches {
+		headerList = append(headerList, match[1])
+		// fmt.Println(" -", match[1])
+	}
+	// fmt.Print(headerList)
+	return headerList, nil
+}
+
+func (g *Grader) ValidationSourceCodeByRegex(fileName string, fileType string, regexCmd string) ([]string, error) {
+	code, err := os.ReadFile(filepath.Join(g.pathOfSource, fileName+"."+fileType))
+	if err != nil {
+		return nil, fmt.Errorf("❌ Error reading source code %s: %v", fileName, err)
+	}
+	// includeRegex := regexp.MustCompile(`#include\s*[<"]([^>"]+)[>"]`)
+	includeRegex := regexp.MustCompile(regexCmd)
+
+	matches := includeRegex.FindAllStringSubmatch(string(code), -1)
+	var regex_list []string
+	// แสดงรายการ Header ที่พบ
+	// fmt.Println("🔍 Files RegEx in " + fileName + " :")
+	for _, match := range matches {
+		// fmt.Println(" -", match[1])
+		regex_list = append(regex_list, match[1])
+	}
+	return regex_list, nil
+}
